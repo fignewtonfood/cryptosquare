a4aaf9a09b613a0e983836b6e84ff009590b0177
Tim White & Nathan Young <student@epicodus.com>
2015-09-08 16:56:03 -0700

    adds refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 var parser = function(phrase, round, start, length) {
     for(start; start < round; start++) {
-        if ((start < length) && (length > 3)) {
-            var output = phrase.charAt(start) + parser(phrase, round, (start+round), length);
-
-        return output;
+        console.log(phrase);
+        console.log(round);
+        console.log(start);
+        console.log(length);
+        if ((parseInt(start) < parseInt(length)) && (parseInt(length) > 3)) {
+            return phrase.charAt(start) + parser(phrase, round, (start+round), length);
+            console.log(phrase);
+            console.log(round);
+            console.log(start);
+            console.log(length);
         } else {
             return phrase;
         }
@@ -14,19 +20,12 @@ var encrypt = function(message) {
     var noPunctuation = message.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     var noSpace = noPunctuation.replace(/\s/g,"");
     var phrase = noSpace.toLowerCase();
-
-
     var length = phrase.length;
     var root = Math.sqrt(length);
     var round = Math.floor(root);
     var start = 0;
     return parser(phrase, round, start, length);
-
 };
-
-
-
-
 
 // $(document).ready(function() {
 //     $("form#factorial").submit(function(event) {
